08c70994d2951c5f9ffb5cde2b39af8b89fafb37
harshvardhansharma2105 <harshvardhan.2105sharma@gmail.com>
2025-11-02 12:14:33 +0530

    updated code

--- SimpleApi.js
+++ SimpleApi.js
@@ -15,4 +15,4 @@ http.createServer((req, resp)=>{
     resp.writeHead(200, {"Content-type" : 'application/json'}); //we use writeHead to to communicate with browser what to show if no error and what to show if there is any error,
     resp.write(JSON.stringify(userData[0].name));
     resp.end();
-}).listen(4000);
+}).listen(4000); 
